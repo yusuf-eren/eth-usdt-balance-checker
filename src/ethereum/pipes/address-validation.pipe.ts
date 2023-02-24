3610a3ba9a5d59b765772ac7cb2f037eb2b33e96
Yusuf Eren <yusuf.e@mindbehind.com>
2023-02-25 01:57:53 +0300

    feat: pipe for ethereum/get-balances endpoint

--- src/ethereum/pipes/address-validation.pipe.ts
+++ src/ethereum/pipes/address-validation.pipe.ts
@@ -1,16 +1,24 @@
-import { Injectable, PipeTransform } from '@nestjs/common';
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 import { isAddress } from 'ethers';
+import { AddressesDto } from '../dtos/addresses.dto';
 
 @Injectable()
 export class AddressValidationPipe implements PipeTransform {
-  transform(addresses: string[]) {
+  transform(addresses: any): AddressesDto {
+    if (!Array.isArray(addresses))
+      throw new BadRequestException('Body should be an array');
+
     const validAddresses = [];
     const invalidAddresses = [];
 
     for (const address of addresses) {
-      if (isAddress(address)) validAddresses.push(address);
-      else invalidAddresses.push(address);
+      isAddress(address)
+        ? validAddresses.push(address)
+        : invalidAddresses.push(address);
     }
+
+    if (validAddresses.length === 0)
+      throw new BadRequestException('No valid addresses provided');
 
     return { validAddresses, invalidAddresses };
   }
