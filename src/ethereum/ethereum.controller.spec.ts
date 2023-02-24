5a9ff00924ede23c92c880a28b8472e13b2e0bdb
Yusuf Eren <yusuf.e@mindbehind.com>
2023-02-25 02:36:39 +0300

    eth controller test inital

--- src/ethereum/ethereum.controller.spec.ts
+++ src/ethereum/ethereum.controller.spec.ts
@@ -1,11 +1,16 @@
+import { HttpModule } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
+import { HelpersModule } from '../helpers/helpers.module';
 import { EthereumController } from './ethereum.controller';
+import { EthereumService } from './ethereum.service';
 
 describe('EthereumController', () => {
   let controller: EthereumController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, HelpersModule],
+      providers: [EthereumService],
       controllers: [EthereumController],
     }).compile();
 
