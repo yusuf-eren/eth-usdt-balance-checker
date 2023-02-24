ee0e384941105a56d6118052af791be064276dd7
Yusuf Eren <yusuf.e@mindbehind.com>
2023-02-25 01:56:20 +0300

    feat: helpersModule import

--- src/ethereum/ethereum.module.ts
+++ src/ethereum/ethereum.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { EthereumService } from './ethereum.service';
 import { EthereumController } from './ethereum.controller';
+import { HelpersModule } from '../helpers/helpers.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, HelpersModule],
   providers: [EthereumService],
   controllers: [EthereumController],
 })
