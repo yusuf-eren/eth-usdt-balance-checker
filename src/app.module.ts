ab99fd1fb7907814970e94cad4e6c7ffd31ac7f2
Yusuf Eren <yusuf.e@mindbehind.com>
2023-02-25 02:08:27 +0300

    fix: unused files removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EthereumModule } from './ethereum/ethereum.module';
 import { HelpersModule } from './helpers/helpers.module';
 
 @Module({
   imports: [EthereumModule, HelpersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
