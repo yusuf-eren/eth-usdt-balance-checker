906e283178a770831394b01b0db7817935bb4d48
Yusuf Eren <yusuf.e@mindbehind.com>
2023-02-25 01:55:21 +0300

    feat: helpers module imported

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EthereumModule } from './ethereum/ethereum.module';
+import { HelpersModule } from './helpers/helpers.module';
 
 @Module({
-  imports: [EthereumModule],
+  imports: [EthereumModule, HelpersModule],
   controllers: [AppController],
   providers: [AppService],
 })
