f103720ebbba314b21a56f7b58b7de73044c0a3f
Yusuf Eren <yusuf.e@mindbehind.com>
2023-02-25 01:56:00 +0300

    feat: controller logic completed

--- src/ethereum/ethereum.controller.ts
+++ src/ethereum/ethereum.controller.ts
@@ -1,34 +1,44 @@
 import {
   Body,
   Controller,
+  HttpCode,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { HelpersService } from '../helpers/helpers.service';
+import { AddressesDto } from './dtos/addresses.dto';
 import { EthereumService } from './ethereum.service';
 import { AddressValidationPipe } from './pipes/address-validation.pipe';
 
 @Controller('ethereum')
 export class EthereumController {
-  constructor(private ethereumService: EthereumService) {}
+  constructor(
+    private readonly ethereumService: EthereumService,
+    private readonly helpersService: HelpersService,
+  ) {}
 
-  @Post('get-balance')
-  @UsePipes(new ValidationPipe({ transform: true }), AddressValidationPipe)
-  async getBalances(@Body() body: any) {
-    const { invalidAddresses, validAddresses } = body;
-    const balancesWithAddresses = await this.ethereumService.checkBalances(
+  @Post('get-balances')
+  @HttpCode(200)
+  @UsePipes(
+    AddressValidationPipe,
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  )
+  async getBalances(@Body() body: AddressesDto) {
+    const { invalidAddresses: invalid_addresses, validAddresses } = body;
+
+    const exchangeRate = await this.ethereumService.ethToUsd();
+    const balancesWithAddresses = await this.ethereumService.getBalances(
       validAddresses,
+      exchangeRate,
+    );
+    const sortedAddresses = this.helpersService.sortAddressesByBalance(
+      balancesWithAddresses,
     );
 
-    return balancesWithAddresses;
-
-    // const sortedAddresses = this.ethereumService.sortAddresses(
-    //   balancesWithAddresses,
-    // );
-    // console.log(balancesWithAddresses)
-    // return {
-    //   invalid_addresses: invalidAddresses,
-    //   sorted_addresses: sortedAddresses,
-    // };
+    return { invalid_addresses, sorted_addresses: sortedAddresses };
   }
 }
