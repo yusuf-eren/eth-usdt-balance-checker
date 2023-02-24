fd864ade04f6b7c1b42836d2d727b12e3bc52436
Yusuf Eren <yusuf.e@mindbehind.com>
2023-02-25 02:36:28 +0300

    feat: eth service isolated tests completed

--- src/ethereum/ethereum.service.spec.ts
+++ src/ethereum/ethereum.service.spec.ts
@@ -1,4 +1,7 @@
+import { HttpModule } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
+import { HelpersModule } from '../helpers/helpers.module';
+import { EthereumController } from './ethereum.controller';
 import { EthereumService } from './ethereum.service';
 
 describe('EthereumService', () => {
@@ -6,7 +9,9 @@ describe('EthereumService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, HelpersModule],
       providers: [EthereumService],
+      controllers: [EthereumController],
     }).compile();
 
     service = module.get<EthereumService>(EthereumService);
@@ -14,5 +19,29 @@ describe('EthereumService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should exchange eth to usd', async () => {
+    const exchangeRate = await service.ethToUsd();
+
+    expect(exchangeRate).toBeDefined();
+    expect(exchangeRate).toEqual(expect.any(Number));
+  });
+
+  it('should ', async () => {
+    const balancesWithAddresses = await service.getBalances(
+      [
+        '0x71C7656EC7ab88b098defB751B7401B5f6d8976F',
+        '0xb794f5ea0ba39494ce839613fffba74279579268',
+        '0xfeC4A115911A120FdEd09a025b32D51e965E1214',
+      ],
+      1,
+    );
+
+    for (const object of balancesWithAddresses) {
+      expect(object.usd_balance).toBeDefined();
+      expect(object.eth_balance).toBeDefined();
+      expect(object.address).toBeDefined();
+    }
   });
 });
