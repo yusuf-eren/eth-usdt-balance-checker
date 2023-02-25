92152bc40f27ef6c66c47f70328ab36f37c76934
Yusuf Eren <yusuf.e@mindbehind.com>
2023-02-25 14:52:31 +0300

    feat: fetch eth balance of addresses

---
+++ src/crypto/ethereum.service.ts
@@ -0,0 +1,30 @@
+import { HttpService } from '@nestjs/axios';
+import { HttpException, Injectable } from '@nestjs/common';
+import { ethers } from 'ethers';
+
+@Injectable()
+export class EthereumService {
+  async getBalances(addresses: string[], exchangeRate: number) {
+    const promises = [];
+
+    const provider = new ethers.InfuraProvider('mainnet');
+
+    for (const address of addresses) {
+      const balance = provider
+        .getBalance(address)
+        .then((balance) => {
+          const eth_balance = parseFloat(ethers.formatEther(balance));
+          return {
+            address,
+            eth_balance,
+            usd_balance: eth_balance * exchangeRate,
+          };
+        })
+        .catch((error) => console.log(error));
+
+      promises.push(balance);
+    }
+
+    return await Promise.all(promises);
+  }
+}
